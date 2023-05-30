2002edff165ccfc1f2a3874d7a4790347333475b
Priya Patel <62372226+priya1523@users.noreply.github.com>
2023-05-30 17:10:34 +0530

    Java new2 (#3)
    * java-new1
    
    docker file changes
    
    * java-new2
    
    spring version chnage and java also

--- Post/src/main/java/com/feuji/post/dto/Image.java
+++ Post/src/main/java/com/feuji/post/dto/Image.java
@@ -1,12 +1,13 @@
 package com.feuji.post.dto;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
